f1dcc0e0f9acfb7348565db6f9dd9e6d607e66cf
romulo.banares <romulo88@hotmail.com>
2024-10-20 00:36:54 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://astro-app-romu.netlify.app',
-  integrations: [react(), solidJs()]
+  integrations: [react(), solidJs()],
+  adapter: netlify()
 });
